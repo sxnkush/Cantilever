b02d940d14a8b5b544632469c9d4eca532322e7d
sxnkush <saxenakushagra198@gmail.com>
2025-07-03 20:54:54 +0530

    Added Loader

--- frontend/Task Manager/src/App.jsx
+++ frontend/Task Manager/src/App.jsx
@@ -1,17 +1,34 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import Home from "./Home";
+import { useState } from "react";
+
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("uid");
+    if (token) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+    setIsLoading(false);
+  }, []);
+
+  if (isLoading) return <div>Loading...</div>;
+
   return (
     <Router>
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={isAuthenticated? <Home />: <Navigate to="/login"/>} />
       </Routes>
     </Router>
   );
 }
 
-export default App
+export default App;
