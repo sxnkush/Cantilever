049f879dc52d7564a62d841f50bf5223b28ed35d
sxnkush <saxenakushagra198@gmail.com>
2025-07-03 22:06:27 +0530

    Final CHnages

--- frontend/Task Manager/src/App.jsx
+++ frontend/Task Manager/src/App.jsx
@@ -1,22 +1,38 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import Home from "./Home";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const BASE_URL = import.meta.env.VITE_API_URL;
 
-  //To avoid flashing of Home on visiting url even if user is not looged in
   useEffect(() => {
-    const token = localStorage.getItem("uid");    //if i get token form localStorage then it implies that user if logged in and Home can be rendered, until this check, Loading.. will appear
-    if (token) {
-      setIsAuthenticated(true);
-    } else {
-      setIsAuthenticated(false);
-    }
-    setIsLoading(false);
+    const checkUser = async () => {
+      try {
+        const res = await axios.get(`${BASE_URL}/api/user`, {
+          withCredentials: true,
+        });
+        if (res.data && res.data._id) {
+          setIsAuthenticated(true);
+        } else {
+          setIsAuthenticated(false);
+        }
+      } catch (err) {
+        console.log("Error checking auth:", err);
+        setIsAuthenticated(false);
+      }
+      setIsLoading(false);
+    };
+
+    checkUser();
   }, []);
 
   if (isLoading) return <div>Loading...</div>;
@@ -26,10 +42,15 @@ function App() {
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={isAuthenticated? <Home />: <Navigate to="/login"/>} />
+        <Route
+          path="/"
+          element={isAuthenticated ? <Home /> : <Navigate to="/login" />}
+        />
       </Routes>
     </Router>
   );
+
+  //Here we are making authentication but still we need to authenticate in Home.jsx, since here we only check whether token is present or not, it may be a expired token but still renders the Home if we remove authentication in Home.jsx. So if token will be expired and we use authentication in HOme as well then it will redirect to /login since it will found no such user with the expired token
 }
 
 export default App;
