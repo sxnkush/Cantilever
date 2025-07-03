bd718bde6d5824215b02af9c4ced7ed7b1881722
sxnkush <saxenakushagra198@gmail.com>
2025-07-03 22:09:04 +0530

    Final CHnages

--- frontend/Task Manager/src/App.jsx
+++ frontend/Task Manager/src/App.jsx
@@ -10,47 +10,15 @@ import Home from "./Home";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
-  const BASE_URL = import.meta.env.VITE_API_URL;
-
-  useEffect(() => {
-    const checkUser = async () => {
-      try {
-        const res = await axios.get(`${BASE_URL}/api/user`, {
-          withCredentials: true,
-        });
-        if (res.data && res.data._id) {
-          setIsAuthenticated(true);
-        } else {
-          setIsAuthenticated(false);
-        }
-      } catch (err) {
-        console.log("Error checking auth:", err);
-        setIsAuthenticated(false);
-      }
-      setIsLoading(false);
-    };
-
-    checkUser();
-  }, []);
-
-  if (isLoading) return <div>Loading...</div>;
-
   return (
     <Router>
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route
-          path="/"
-          element={isAuthenticated ? <Home /> : <Navigate to="/login" />}
-        />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
-
-  //Here we are making authentication but still we need to authenticate in Home.jsx, since here we only check whether token is present or not, it may be a expired token but still renders the Home if we remove authentication in Home.jsx. So if token will be expired and we use authentication in HOme as well then it will redirect to /login since it will found no such user with the expired token
 }
 
 export default App;
